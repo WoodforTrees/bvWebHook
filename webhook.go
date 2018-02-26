fa5af283a2be5790e01c3f269fbe4febcab0901c
Joe Humphries <joe.humphries75@gmail.com>
2018-02-26 21:03:59 +0000

    set correct UK date format

--- webhook.go
+++ webhook.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
-        "time"
+	"time"
 )
 
 func main() {
@@ -14,20 +14,20 @@ func main() {
 	name := os.Getenv("VIRTUSER")
 	fileLocation := os.Getenv("SSHUPLOADFILE")
 	fileSize := os.Getenv("SSHUPLOADBYTES")
-	currentTime := time.Now().Format(time.RFC3339)
+	currentTime := time.Now().Format("02/01/2006 15:04:05") //This is how you do dates in go.. they are magic numbers
 	fileLocation = strings.Replace(fileLocation, "\\", "/", -1)
 
-	log.Println("Version 1.0")
+	log.Println("Version 0.3")
 
 	payloadRaw := "{\"@type\":\"MessageCard\",\"@context\":\"http://schema.org/extensions\",\"Summary\":\"Testing\",\"themeColor\":\"00ff00\",\"sections\":[{\"activityTitle\":\"New Data has arrived on the FTP Server\",\"facts\":[{\"name\":\"User:\",\"value\":\"XXXX\"},{\"name\":\"File:\",\"value\":\"YYYY\"},{\"name\":\"Size:\",\"value\":\"ZZZZ\"},{\"name\":\"Date:\",\"value\":\"TTTT\"}]}]}"
-
 	payloadRaw = strings.Replace(payloadRaw, "XXXX", name, 1)
 	payloadRaw = strings.Replace(payloadRaw, "YYYY", fileLocation, 1)
 	payloadRaw = strings.Replace(payloadRaw, "ZZZZ", fileSize, 1)
+	payloadRaw = strings.Replace(payloadRaw, "TTTT", currentTime, 1)
 
 	log.Println(payloadRaw)
 
-	url := "https://outlook.office.com/webhook/......"
+	url := "https://outlook.office.com/webhook/686a7071-f9ea-4fca-865e-45d3c8f674b2@74bc964f-1d6e-4ec4-8ec5-4c77c63906a9/IncomingWebhook/3c2eadeb7b734d7cb5ef477cc1f864bf/b4f61c5c-830a-4995-b490-8ed756ea5fc0"
 
 	log.Println("Posting..")
 
@@ -43,5 +43,6 @@ func main() {
 	ioutil.ReadAll(res.Body)
 
 	log.Println(res)
+
 }
 
