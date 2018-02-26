cff73846c754c96e494b110f151dee4ee1ac7cdf
Joe Humphries <joe.humphries75@gmail.com>
2018-02-26 21:07:10 +0000

    set version and date to correct UK format

--- webhook.go
+++ webhook.go
@@ -17,7 +17,7 @@ func main() {
 	currentTime := time.Now().Format("02/01/2006 15:04:05") //This is how you do dates in go.. they are magic numbers
 	fileLocation = strings.Replace(fileLocation, "\\", "/", -1)
 
-	log.Println("Version 0.3")
+	log.Println("Version 1.1")
 
 	payloadRaw := "{\"@type\":\"MessageCard\",\"@context\":\"http://schema.org/extensions\",\"Summary\":\"Testing\",\"themeColor\":\"00ff00\",\"sections\":[{\"activityTitle\":\"New Data has arrived on the FTP Server\",\"facts\":[{\"name\":\"User:\",\"value\":\"XXXX\"},{\"name\":\"File:\",\"value\":\"YYYY\"},{\"name\":\"Size:\",\"value\":\"ZZZZ\"},{\"name\":\"Date:\",\"value\":\"TTTT\"}]}]}"
 	payloadRaw = strings.Replace(payloadRaw, "XXXX", name, 1)
@@ -27,7 +27,7 @@ func main() {
 
 	log.Println(payloadRaw)
 
-	url := "https://outlook.office.com/webhook/686a7071-f9ea-4fca-865e-45d3c8f674b2@74bc964f-1d6e-4ec4-8ec5-4c77c63906a9/IncomingWebhook/3c2eadeb7b734d7cb5ef477cc1f864bf/b4f61c5c-830a-4995-b490-8ed756ea5fc0"
+	url := "https://outlook.office.com/webhook/......"
 
 	log.Println("Posting..")
 
